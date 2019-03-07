b2c309649e0c2fd7234584b3adeb43da3a98f6c2
Omar Madriz <madriz_omar@hotmail.com>
2019-03-06 20:46:02 -0600

    matrixOps ended, includes matrix multiplication, matrix transpose and gauss elimination method

--- matrixOps.py
+++ matrixOps.py
@@ -1,5 +1,3 @@
-import load_data as ld
-
 def mult_matrix(A, B):
     res = []
     if(len(A[0]) != len(B)):
@@ -10,7 +8,8 @@ def mult_matrix(A, B):
             for j in range(len(B[0])):
                 for k in range(len(B)):
                     res[i][j] += (A[i][k] * B[k][j])
-    return res 
+    return res
+
 
 def transMatrix(A):
     AT = []
@@ -20,20 +19,37 @@ def transMatrix(A):
             AT[i][j] = A[j][i]
     return AT
 
-ld.create_sample('new_file.csv', 'airfoil_self_noise_.csv', 10)
-A, Y = ld.createRegMatrix('new_file.csv')
-AT = transMatrix(A)
-for i in range(len(A)):
-    print(A[i])
-for i in range(len(AT)):
-    print(AT[i])
 
-ATA = mult_matrix(AT,A)
-for i in range(len(ATA)):
-    print(ATA[i])
-'''
-C = [[1,1,1,1],[2,3,5,6],[4,9,25,36]]
-D = [[1,2,4],[1,3,9],[1,5,25], [1,6,36]]
+def gauss_solve(A):
+    """Gaussian eliminator method  Author: j9ac9k (github member)
+    url: https://gist.github.com/j9ac9k/6b5cd12aa9d2e5aa861f942b786293b4"""
+    m = len(A)
+    assert all([len(row) == m + 1 for row in A[1:]]), "Matrix rows have non-uniform length"
+    n = m + 1
+    
+    for k in range(m):
+        pivots = [abs(A[i][k]) for i in range(k, m)]
+        i_max = pivots.index(max(pivots)) + k
+        
+        # Check for singular matrix
+        assert A[i_max][k] != 0, "Matrix is singular!"
+        
+        # Swap rows
+        A[k], A[i_max] = A[i_max], A[k]
+        
+        for i in range(k + 1, m):
+            f = A[i][k] / A[k][k]
+            for j in range(k + 1, n):
+                A[i][j] -= A[k][j] * f
 
-result = mult_matrix(C,D)
-'''
+            # Fill lower triangular matrix with zeros:
+            A[i][k] = 0
+    
+    # Solve equation Ax=b for an upper triangular matrix A         
+    x = []
+    for i in range(m - 1, -1, -1):
+        x.insert(0, A[i][m] / A[i][i])
+        for k in range(i - 1, -1, -1):
+            A[k][m] -= A[k][i] * x[0]
+    return x
+
