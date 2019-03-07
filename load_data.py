0fbbe96f5cf436643517cb7b76de4ae4771c06aa
Omar Madriz <madriz_omar@hotmail.com>
2019-03-07 13:06:33 -0600

    createSets function ready to create the train and test sets into a csv file

--- load_data.py
+++ load_data.py
@@ -2,16 +2,15 @@ import csv
 import random as rd
 
 def create_sample(src, dest, percentage):
-    with open(src, 'w', newline="") as new_file:
+    with open(dest, 'w', newline="") as new_file:
         new_file_writer = csv.writer(new_file)
-        with open(dest, 'r') as csv_file:
+        with open(src, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
             line_count = 0
             for row in csv_reader:
                 rand = rd.randint(0, 100) / 100
                 if(rand <= percentage/100):
                     new_file_writer.writerow(row)
-                    print(row)
                     line_count += 1
     csv_file.close()
     new_file.close()
@@ -25,8 +24,27 @@ def createRegMatrix(src):
         csv_reader = csv.reader(csv_sample, delimiter="\t")
         for row in csv_reader:
             A.append([1] + [float(e) for e in row[:-1]])
-            Y.append(float(row[-1]))
+            Y.append([float(row[-1])])
     return A, Y   
 
-
-
+def createSets(src, train_percentage):
+    with open("train_set", "w", newline="") as train_file:
+        train_writer = csv.writer(train_file)
+        with open("test_set", "w", newline="") as test_file:
+            test_writer = csv.writer(test_file)
+            with open(src, "r") as sample_file:
+                sample_reader = csv.reader(sample_file)
+                count_train = 0
+                count_test = 0
+                for row in sample_reader:
+                    rand = rd.randint(0,100) / 100
+                    if(rand <= train_percentage/100):
+                        train_writer.writerow(row)
+                        count_train +=1
+                    else:
+                        test_writer.writerow(row)
+                        count_test +=1
+    print(f"Train set created with {count_train} lines \n Test set created with {count_test} lines")
+    train_file.close()
+    test_file.close()
+    sample_file.close()
