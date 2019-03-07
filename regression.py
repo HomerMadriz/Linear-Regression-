a216267ca308bcc41936ca46e29c640c3161ae56
Omar Madriz <madriz_omar@hotmail.com>
2019-03-07 13:08:41 -0600

    Linear regression done

--- regression.py
+++ regression.py
@@ -1,8 +1,25 @@
 import load_data as ld
 import matrixOps as mops
 
-def linear_regression(data_file,data_percentaje):
-    ld.create_sample('new_file.csv', data_file, data_percentaje)
-    A, Y = ld.createRegMatrix('new_file.csv')
-    AT = mops.transMatrix(A)
-    ATA = mops.mult_matrix(AT,A)
+def linear_regression(data_file):
+    A, Y = ld.createRegMatrix('data_file')
+    At = mops.transMatrix(A)
+    AtA = mops.mult_matrix(At,A)
+    AtY = mops.mult_matrix(At, Y)
+
+    for i in range(len(AtY)):
+        AtA[i].append(AtY[i][0])
+
+    mops.printMatrix(AtA)
+    print()
+    mops.printMatrix(AtY)
+
+    b_vector = mops.gauss_solve(AtA)
+
+    mops.printMatrix(b_vector)
+
+    return b_vector
+
+def test(test_file, b_vector):
+    A, Y = ld.createRegMatrix(test_file)
+    
